55454f1c996f16a7279bcea19e5aee9a82d59c78
WaelS87 <wfakhrji@gmail.com>
2022-07-04 15:34:31 -0300

    subo el proyecto

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const port = 3030;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
+
+
+
+app.listen(port, () => console.log('Servidor corriendo en http://localhost:' + port));
